f69312621f8f9d641b8419f2760d157569be6594
KonradWrbl <160815@stud.prz.edu.pl>
2019-05-30 22:32:48 +0200

    tests...

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({cityList: JSON.parse(localStorage.getItem('cityList'))})
+    this.setState({cityList: JSON.parse(localStorage.getItem('cityList')) ? JSON.parse(localStorage.getItem('cityList')) : []})
   }
 
   addCity = () => {
